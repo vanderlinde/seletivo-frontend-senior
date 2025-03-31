c372a0190ffd88a6e0872b3a2058fdac9b520c62
Marcelo Vanderlinde Júnior <mvanderlindejr@gmail.com>
2025-03-31 13:36:13 -0400

    Criando módulo home

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+import { CardPersonComponent } from './components/card-person/card-person.component';
+import { CardSkeletonComponent } from './components/card-skeleton/card-skeleton.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    ReactiveFormsModule,
+    CardPersonComponent,
+    CardSkeletonComponent
+  ]
+})
+export class HomeModule { }
---
+++ src/app/home/home.ts
@@ -0,0 +1,76 @@
+export class HomeSearch {
+    nome?: string;
+    faixaIdadeInicial?: number;
+    faixaIdadeFinal?: number;
+    sexo?: Sexo;
+    status?: StatusDesaparecido;
+}
+
+export class ResultadoPaginado {
+    totalPages?: number;
+    totalElements?: number;
+    last?: boolean;
+    numberOfElements?: number;
+    first?: boolean;
+    size?: number;
+    content?: Array<Desaparecido>;
+}
+
+export class Desaparecido {
+    id?: number;
+    nome?: string;
+    idade?: number;
+    sexo?: Sexo;
+    vivo?: boolean;
+    urlFoto?: string;
+    ultimaOcorrencia?: IUltimaOcorrencia;
+  }
+
+export interface IUltimaOcorrencia {
+    dtDesaparecimento: string;
+    dataLocalizacao: any;
+    encontradoVivo: boolean;
+    localDesaparecimentoConcat: string;
+    ocorrenciaEntrevDesapDTO: IOcorrenciaEntrevDesapDto;
+    listaCartaz: any;
+    ocoId: number;
+}
+
+export interface IOcorrenciaEntrevDesapDto {
+  informacao: any;
+  vestimentasDesaparecido: string;
+}
+
+export interface IPaginacaoRequest {
+    pagina: number,
+    porPagina: number;
+}
+
+export const ListaSexos = {
+    MASCULINO: 'Masculino',
+    FEMININO: 'Feminino',
+};
+
+export const ListaStatusDesaparecido = {
+    LOCALIZADO: 'Localizado',
+    DESAPARECIDO: 'Desaparecido',
+};
+
+export class Estatisticas {
+    quantPessoasDesaparecidas: number = 0;
+    quantPessoasEncontradas: number = 0;
+}
+export interface IEstatisticas {
+    quantPessoasDesaparecidas: number;
+    quantPessoasEncontradas: number;
+}
+
+  export interface IPaginacaoRequest {
+    pagina: number,
+    porPagina: number;
+}
+
+
+export type Resultado = keyof typeof ResultadoPaginado;
+export type Sexo = keyof typeof ListaSexos;
+export type StatusDesaparecido = keyof typeof ListaStatusDesaparecido;
