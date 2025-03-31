d059595bd526b91d70495b55120adfc7f60929c8
Marcelo Vanderlinde Júnior <mvanderlindejr@gmail.com>
2025-03-31 13:38:44 -0400

    Criando componente de `detalhes do desaparecido`

---
+++ src/app/detalhes-desaparecido/detalhes-desaparecido/detalhes-desaparecido.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-desaparecido',
+  standalone: false,
+  templateUrl: './detalhes-desaparecido.component.html',
+  styleUrl: './detalhes-desaparecido.component.css'
+})
+export class DetalhesDesaparecidoComponent {
+
+}
