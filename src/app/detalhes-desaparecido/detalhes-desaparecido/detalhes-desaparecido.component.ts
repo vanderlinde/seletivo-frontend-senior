7aa870a1a94f2afca658fcced690787a01c11423
Marcelo Vanderlinde Júnior <mvanderlindejr@gmail.com>
2025-03-31 15:54:15 -0400

    Montando visual da tela `detalhes-desaparecidos`

--- src/app/detalhes-desaparecido/detalhes-desaparecido/detalhes-desaparecido.component.ts
+++ src/app/detalhes-desaparecido/detalhes-desaparecido/detalhes-desaparecido.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pessoa } from '../detalhes-desaparecido';
+import { ListaSexos } from '../../home/home';
+import { ActivatedRoute } from '@angular/router';
+import { DetalhesDesaparecidoService } from '../detalhes-desaparecido.service';
 
 @Component({
   selector: 'app-detalhes-desaparecido',
@@ -6,6 +10,39 @@ import { Component } from '@angular/core';
   templateUrl: './detalhes-desaparecido.component.html',
   styleUrl: './detalhes-desaparecido.component.css'
 })
-export class DetalhesDesaparecidoComponent {
+export class DetalhesDesaparecidoComponent implements OnInit {
+  
+  listaSexo = ListaSexos;
+  loadingImage: boolean = true;
+  loadingDesaparecido: boolean = true;
+  desaparecidoId: string = '0';
+  desaparecido: Pessoa = new Pessoa();
 
+  constructor(
+    private route: ActivatedRoute,
+    private detalhesService: DetalhesDesaparecidoService
+  ) {}
+
+  ngOnInit(): void {
+    this.getDesaparecidoId()
+    this.detalhesService.getDesaparecido(this.desaparecidoId).subscribe((resp) => {
+      this.desaparecido = resp
+      console.log('desaparecido', this.desaparecido)
+    } )
+  }
+
+  getDesaparecidoId(){
+    this.desaparecidoId = this.route.snapshot.paramMap.get('id')!;
+  }
+
+  getDiasDesaparecido(): number {
+    const today = new Date()
+    const dtDesaparecimento = new Date(this.desaparecido.ultimaOcorrencia!.dtDesaparecimento)
+    const diff = today.getTime() - dtDesaparecimento.getTime()
+    return Math.floor(diff / (1000 * 50 * 40 * 24))
+  }
+
+  teste(){
+    console.log('button')
+  }
 }
