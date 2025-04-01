dc12786f08e07966863e3aec7fc51ee0fbabe6e1
Marcelo Vanderlinde Júnior <mvanderlindejr@gmail.com>
2025-03-31 20:46:31 -0400

    Criando `service` do módulo `detalhes-desaparecidos`

--- src/app/detalhes-desaparecido/detalhes-desaparecido.service.ts
+++ src/app/detalhes-desaparecido/detalhes-desaparecido.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { env } from '../../environment/environment'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { IInformacao } from './detalhes-desaparecido';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,26 @@ export class DetalhesDesaparecidoService {
     return this.http.get(url)
   }
 
+  sendInformationDesaparecido(informacao: IInformacao, dialogRef: MatDialogRef<DetalhesDesaparecidoService>){
+    const url = `${env.url}v1/ocorrencias/informacoes-desaparecido`
+    let formData = new FormData();
+    formData.append('informacao', informacao.informacao);
+    formData.append('descricao', '');
+    formData.append('data', informacao.data.toString());
+    formData.append('ocoId', informacao.ocoId.toString());
+    if (!!informacao.files?.length) {
+      informacao.files.forEach((file: File) => {
+        formData.append('files', file);
+      })
+    }
+    return this.http.post(url, formData).subscribe({
+      next: () => {
+        dialogRef.close()
+      },
+      error: (err: any) => {
+        console.error(err)
+      }
+    });
+  }
 
 }
